af6c58e5200141b7d1d92e042cba31476ff6380c
Llane Rost <llanerost@gmail.com>
2020-10-18 00:27:35 -0700

    pep changes

--- pa.py
+++ pa.py
@@ -13,7 +13,12 @@ currDate = datetime.now().strftime("%Y-%m-%d")
 currDay = datetime.now().strftime("%A")
 
 with open(f"{currDate}.csv", "a") as f:
-    writer = csv.DictWriter(f, fieldnames=["Number", "Period", "Due Date", "Time", "Task"])
+    fieldnames = ["Number", "Period", "Due Date", "Time", "Task"]
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
     for line in data:
-        writer.writerow({'Number': line['Number'], 'Period': line['Period'], 'Due Date': line['Due Date'], 'Time': line['Time'], 'Task': line['Task']})
-        f.write("\n")
+        writer.writerow({
+            'Number': line['Number'], 
+            'Period': line['Period'], 
+            'Due Date': line['Due Date'], 
+            'Time': line['Time'], 
+            'Task': line['Task']})
